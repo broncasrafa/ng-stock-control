deac60450c31ae58e625d2d0b5184189ded82eaf
Rafael Francisco <broncasrafa@gmail.com>
2024-11-02 09:54:01 -0300

    👉🏾 adicionado o grafico de produtos em estoque

--- stock-control-front/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ stock-control-front/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
+
 import { Subject, takeUntil } from 'rxjs';
+import { ChartData, ChartOptions } from 'chart.js';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { NotificationService, NotificationType } from 'src/app/services/notification/notification.service';
 import { ProductsDataTransferService } from 'src/app/services/products/products-data-transfer.service';
@@ -13,7 +15,10 @@ import { ProductsService } from 'src/app/services/products/products.service';
 })
 export class DashboardHomeComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
+
   public productsList: Array<GetAllProductsResponse> = [];
+  public productsChartDatas!: ChartData;
+  public productsChartOptions!: ChartOptions;
 
   constructor(
     private productsService: ProductsService,
@@ -32,6 +37,7 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
               if (response && response.length > 0) {
                 this.productsList = response;
                 this.productsDataTransferService.setProductsDatas(this.productsList);
+                this.setProductsChartConfig();
               }
           },
           error: (err) => this.notificationService.showNotificationMessage('Erro', 'Ocorreu um erro ao tentar obter os produtos', NotificationType.ERROR, 3000)
@@ -39,10 +45,65 @@ export class DashboardHomeComponent implements OnInit, OnDestroy {
     }
 
 
+    setProductsChartConfig(): void {
+      if (this.productsList.length > 0) {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
+        this.productsChartDatas = {
+          labels: this.productsList.map((product) => product?.name),
+          datasets: [
+            {
+              label: 'Quantidade',
+              data: this.productsList.map((product) => product?.amount),
+              backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+              borderColor: documentStyle.getPropertyValue('--indigo-400'),
+              hoverBackgroundColor: documentStyle.getPropertyValue('--indigo-500'),
+            }
+          ],
+        }
+
+        this.productsChartOptions = {
+          maintainAspectRatio: false,
+          aspectRatio: 0.8,
+          plugins: {
+            legend: {
+              labels: {
+                color: textColor,
+              },
+            },
+          },
+
+          scales: {
+            x: {
+              ticks: {
+                color: textColorSecondary,
+                font: {
+                  weight: '500',
+                },
+              },
+              grid: {
+                color: surfaceBorder,
+              },
+            },
+            y: {
+              ticks: {
+                color: textColorSecondary,
+              },
+              grid: {
+                color: surfaceBorder,
+              },
+            },
+          },
+        };
+      }
+    }
 
 
     ngOnDestroy(): void {
-
+      this.destroy$.next();
+      this.destroy$.complete();
     }
 }
